7d59f3f864cb992ab1e11c1c6dee2b10b0869c47
츄차원의세계 <lazpberry1012@gmail.com>
2022-06-17 10:09:16 +0900

    Course 2. Intro to Numpy & OpenGL

--- 2. Intro to NumPy & OpenGL/BasicOpenGL.py
+++ 2. Intro to NumPy & OpenGL/BasicOpenGL.py
@@ -1,5 +1,6 @@
 import glfw
 from OpenGL.GL import *
+import numpy as np
 
 def triangle():
     glClear(GL_COLOR_BUFFER_BIT)
@@ -19,8 +20,53 @@ def smallTriangle():
     glVertex2f(0.5, -0.5)
     glEnd()
 
+def primitiveTypes():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    #glBegin(GL_POINTS)
+    #glBegin(GL_LINES)
+    #glBegin(GL_LINE_STRIP)
+    glBegin(GL_LINE_LOOP)
+
+    glVertex2f(0.0, 0.5)
+    glVertex2f(-0.5, -0.5)
+    glVertex2f(0.5, -0.5)
+    glEnd()
+
+def rainbowTriangle():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glBegin(GL_TRIANGLES)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex2f(0.0, 1.0)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex2f(-1.0, -1.0)
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex2f(1.0, -1.0)
+    glEnd()
+
+def redTriangle():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glBegin(GL_TRIANGLES)
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex2f(0.0, 1.0)
+    glVertex2f(-1.0, -1.0)
+    glVertex2f(1.0, -1.0)
+    glEnd()
+
+def redTriangle_otherForm():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glBegin(GL_TRIANGLES)
+    glColor3ub(255, 0, 0)
+    glVertex2fv((0.0, 1.0))
+    glVertex2fv([-1.0, -1.0])
+    glVertex2fv(np.array([1.0, -1.0]))
+    glEnd()
+
 def render():
-    smallTriangle()
+    redTriangle_otherForm()
 
 def main():
     if not glfw.init():
