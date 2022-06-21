431f846c25096e2c5c279693be4b4b2c240904ea
나란히 <lazpberry1012@gmail.com>
2022-06-21 22:55:19 +0900

    2D Transform Animation

--- 3. Transformation 1/2D Transform.py
+++ 3. Transformation 1/2D Transform.py
@@ -28,29 +28,61 @@ def uniform_scale(M):
 def render(M):
     uniform_scale(M)
 
+# def main():
+#     if not glfw.init():
+#         return
+
+#     window = glfw.create_window(640, 640, "2D Trans", None, None)
+#     if not window:
+#         glfw.terminate()
+#         return
+
+#     glfw.make_context_current(window)
+
+#     while not glfw.window_should_close(window):
+#         glfw.poll_events()
+
+#         M = np.array([
+#             [2., 0.],
+#             [0., 2.]
+#         ])
+
+#         render(M)
+
+#         glfw.swap_buffers(window)
+    
+#     glfw.terminate()
+
+# Animation
+
 def main():
     if not glfw.init():
         return
-
-    window = glfw.create_window(640, 640, "2D Trans", None, None)
+    window = glfw.create_window(640,640,"2D Trans", None,None)
     if not window:
         glfw.terminate()
         return
-
     glfw.make_context_current(window)
+
+    # set the number of screen refresh to wait before calling glfw.swap_buffer().
+    # if your monitor refresh rate is 60Hz, the while loop is repeated every 1/60 sec
+    glfw.swap_interval(1)
 
     while not glfw.window_should_close(window):
         glfw.poll_events()
 
+        # get the current time, in seconds
+        t = glfw.get_time()
+
+        s = np.sin(t)
         M = np.array([
-            [2., 0.],
-            [0., 2.]
+            [s , 0.],
+            [0., s ]
         ])
 
         render(M)
 
         glfw.swap_buffers(window)
-    
     glfw.terminate()
 
 if __name__ == "__main__":
